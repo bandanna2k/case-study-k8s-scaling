2753111d488c8720b0254d705586d57c8d012ffb
David North <bandanna2k@gmail.com>
2025-09-26 13:19:07 +1200

    Vertx server only

---
+++ load-generator/src/main/java/dnt/loadgenerator/Server.java
@@ -0,0 +1,60 @@
+package dnt.loadgenerator;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import static io.vertx.ext.auth.audit.impl.SecurityAuditLogger.LOGGER;
+
+public class Server implements AutoCloseable
+{
+    private final Vertx vertx;
+
+    public Server(Vertx vertx)
+    {
+        this.vertx = vertx;
+    }
+
+    public Future<HttpServer> start()
+    {
+        Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+
+        return vertx.createHttpServer()
+                .requestHandler(router)
+                .listen(10200)
+                .onSuccess(successfulHttpServer -> {
+                    LOGGER.info("Server started on port " + successfulHttpServer.actualPort());
+                })
+                .onFailure(t -> LOGGER.error("Failed to start server", t));
+    }
+
+    @Override
+    public void close()
+    {
+        vertx.close();
+    }
+
+    public static void main(String[] args)
+    {
+        try(Server server = new Server(VertxFactory.newVertx());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8)))
+        {
+            server.start();
+            Thread.sleep(2_000);
+            System.out.println("Press any to exit.");
+            reader.readLine();
+        }
+        catch (IOException | InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ load-generator/src/main/java/dnt/loadgenerator/VertxFactory.java
@@ -0,0 +1,25 @@
+package dnt.loadgenerator;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class VertxFactory
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(VertxFactory.class);
+
+    public static Vertx newVertx()
+    {
+        VertxOptions options = new VertxOptions();
+        Vertx vertx = Vertx.vertx(options);
+
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            LOGGER.info("Executing custom shutdown hook... {}", vertx.hashCode());
+            vertx.close();
+            LOGGER.info("Executed custom shutdown hook...{}", vertx.hashCode());
+        }));
+        return vertx;
+    }
+}
