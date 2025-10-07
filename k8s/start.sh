bcbb3d46ae25c64c3806579469b0eaa7cc4a3511
David North <bandanna2k@gmail.com>
2025-10-07 23:00:46 +1300

    feat: upload load-generator to minikube

--- k8s/start.sh
+++ k8s/start.sh
@@ -9,6 +9,9 @@ minikube start
 minikube addons enable metrics-server
 minikube kubectl -- wait pod --all --for=condition=Ready --namespace=kube-system --timeout=60s
 
+# Upload into minikube, the load generator image
+minikube image load load-generator
+
 source apply.sh
 minikube kubectl -- wait pod --all --for=condition=Ready --namespace=case-study --timeout=120s
 
